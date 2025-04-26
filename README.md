5f6275d54c12c31cdc08009548ae16c6159b9456
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2025-04-26 13:36:17 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SrijanPaulsatya_26-04-2025
---
+++ fetch.sh
@@ -0,0 +1,5 @@
+url="https://storage.googleapis.com/hiring-problem-statements/store-monitoring-data.zip"
+
+curl $url -O
+
+unzip store-monitoring-data.zip -d data
